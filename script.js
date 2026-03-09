2e3ccbac43ab12bd3ce076ae13e22b3bc1abad26
KanizSanjila <kanizsm13@gmail.com>
2026-03-09 19:34:44 +0600

    display issues card

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function login(){
 const user = document.getElementById("username").value
 const pass = document.getElementById("password").value
 
-if(user === "0" && pass === "0"){
+if(user === "admin" && pass === "admin123"){
 
 document.getElementById("loginPage").classList.add("hidden")
 document.getElementById("mainPage").classList.remove("hidden")
